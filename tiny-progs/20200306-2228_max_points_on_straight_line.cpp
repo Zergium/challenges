dbd3c5cc44efb9bcef780cfb45302490a018fe5c
Zergium <grwlfo@gmail.com>
2020-03-06 23:01:12 +0100

    altered finding points on a same line

--- tiny-progs/20200306-2228_max_points_on_straight_line.cpp
+++ tiny-progs/20200306-2228_max_points_on_straight_line.cpp
@@ -1,20 +1,43 @@
+// trick is that some coordinates are toooo large and there are duplicated points
+
 class Solution {
 public:
-    int maxPoints(vector<vector<int>>& points) {
+    int maxPoints(vector<vector<int>>& points_org) {
+        vector<vector<int>> points;
+        for (auto& pnt: points_org) {
+            bool found(false);
+            for (auto& existing: points) {
+                if (pnt[0] == existing[0] && pnt[1] == existing[1]) {
+                    found = true;
+                    existing[2]++;
+                    break;
+                }
+            }
+            if (!found) {
+                points.push_back({pnt[0], pnt[1], 1});
+            }
+        }
+        
+        /// now points has [x, y, num_at_this_point] without duplicates of [x,y]
+                  
         int n(points.size());
         if (n<3) {
-          return n;  
+            int result(0);
+            for (auto pnt: points) {
+                result += pnt[2];
+            }
+            return result;
         }
         int maxPoints = 2;
         for (int i = 0; i < n-2; ++i)
             for (int j = i + 1; j < n-1; ++j) {
-                int numPoints = 2;
-                int dj_x = points[i][0]-points[j][0];
-                int dj_y = points[i][1]-points[j][1];
+                int numPoints = points[i][2] + points[j][2];
+                long long dj_x = points[i][0]-points[j][0];
+                long long dj_y = points[i][1]-points[j][1];
                 
                 for (int k = j + 1; k < n; ++k) {
-                    int dk_x = points[i][0]-points[j][0];
-                    int dk_y = points[i][1]-points[j][1];
+                    long long dk_x = points[i][0]-points[k][0];
+                    long long dk_y = points[i][1]-points[k][1];
                     
                     bool sameLine = false;
                     if (dj_x == 0) {
@@ -29,7 +52,7 @@ public:
                         }
                     }
                     if (sameLine) {
-                        numPoints++;
+                        numPoints += points[k][2];
                     }
                 }
                 
