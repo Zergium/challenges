2102c8e49806737052c75f684ffb9230db2c632f
Zergium <grwlfo@gmail.com>
2020-03-06 20:11:18 +0100

    correction and note about result, word-break

--- word-break/20200306-2007_recursion.cpp
+++ word-break/20200306-2007_recursion.cpp
@@ -1,10 +1,14 @@
+// works, but time limit exceeded :(
+// "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
+//["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         if (s.empty()) return true;
         for (const string& word: wordDict) {
             if (s.substr(0, word.length()) == word) {
-                if (wordBreak(s.substr(word.length()))) 
+                if (wordBreak(s.substr(word.length()), wordDict))
                     return true;
             }
         }
