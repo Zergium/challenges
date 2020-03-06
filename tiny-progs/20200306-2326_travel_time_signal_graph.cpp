3eb2ba1f9d31811350fc98fe838a0919bd2006b8
Zergium <grwlfo@gmail.com>
2020-03-06 23:34:13 +0100

    reworked graph signal structure

--- tiny-progs/20200306-2326_travel_time_signal_graph.cpp
+++ tiny-progs/20200306-2326_travel_time_signal_graph.cpp
@@ -2,6 +2,14 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int N, int K) {
         
+        struct node_t {
+            vector<vector<int>> adjWeight;
+        };
+        vector<node_t> graph(N);
+        for (auto edge: times) {
+            graph[edge[0]-1].adjWeight.push_back({edge[1]-1, edge[2]});
+        }
+        
         vector<int> bestTime(N, -1);
         bestTime[K-1] = 0;
         std::deque<int> nodes;
@@ -9,15 +17,13 @@ public:
         while (!nodes.empty()) {
             int curr = nodes.front();
             nodes.pop_front();
-            for (auto edge: times) {
-                if (edge[0]-1 == curr) {
-                    int link = edge[1]-1;
-                    int weight = edge[2];
-                    if (bestTime[link] < 0 ||
-                        bestTime[link] > bestTime[curr] + weight) {
-                        bestTime[link] = bestTime[curr] + weight;
-                        nodes.push_back(link);
-                    }
+            for (auto next: graph[curr].adjWeight) {
+                int link = next[0];
+                int weight = next[1];
+                if (bestTime[link] < 0 ||
+                    bestTime[link] > bestTime[curr] + weight) {
+                    bestTime[link] = bestTime[curr] + weight;
+                    nodes.push_back(link);
                 }
             }
         }
