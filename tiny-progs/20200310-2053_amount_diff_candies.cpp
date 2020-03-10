1c9561f1aa23334f64a770e300ee03529546372d
Zergium <grwlfo@gmail.com>
2020-03-10 20:57:45 +0100

    fixed compile errors

--- tiny-progs/20200310-2053_amount_diff_candies.cpp
+++ tiny-progs/20200310-2053_amount_diff_candies.cpp
@@ -3,22 +3,22 @@ public:
     int distributeCandies(vector<int>& candies) {
         unordered_map<int, int> amounts;
         for (int candy: candies) {
-            anounts[candy] += 1;
+            amounts[candy] += 1;
         }
         
         vector<pair<int,int>> sorted;
         for (auto candy_amount: amounts) {
             sorted.push_back(candy_amount);
         }
-        sort(sorted, [](const pair<int,int>& a, const pair<int,int>& b) {
+        sort(begin(sorted), end(sorted), [](const pair<int,int>& a, const pair<int,int>& b) -> bool {
             return a.second < b.second;
         });
-        
+
         int idx = 0;
         int amount_for_child = candies.size() /2;
         int counted = 0;
-        while (idx < sorted.size() && sorted[idx].second + amount < amount_for_child) {
-            amount += sorted[idx].second;
+        while (idx < sorted.size() && sorted[idx].second + counted < amount_for_child) {
+            counted += sorted[idx].second;
         };
         
         return idx;
