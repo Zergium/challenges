1730a7c4c55cd4ea12c22b8a5eb640985985c287
Zergium <grwlfo@gmail.com>
2020-03-12 00:26:45 +0100

    added trim BST & delete duplicates from list

---
+++ tiny-progs/20200312-0025_deleteDuplications_from_sorted_list.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode tmp;
+        tmp.next = head;
+        ListNode* checker = &tmp;
+        while (checker != nullptr) {
+            if (nullptr != checker->next && nullptr != checker->next->next) {
+                if (checker->next->val == checker->next->next->val) {
+                    int valToRemove = checker->next->val;
+                    while (nullptr != checker->next && valToRemove == checker->next->val) {
+                        checker->next = checker->next->next;
+                    }
+                }
+                checker = checker->next;
+            }
+        }
+        return tmp.next;
+    }
+};
