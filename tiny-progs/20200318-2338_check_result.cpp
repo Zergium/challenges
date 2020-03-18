9fac4514cbdaa975bf357b03a7cd540536f14688
Zergium <grwlfo@gmail.com>
2020-03-18 23:44:42 +0100

    improved speed of find result

--- tiny-progs/20200318-2338_check_result.cpp
+++ tiny-progs/20200318-2338_check_result.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     
-    void checkResult(vector<int>& candidates, int target, vector<int>& options, vector<vector<int>>& result) {
-        for (int cnd: candidates) {
+    void checkResult(vector<int>& candidates, int target, int pos, vector<int>& options, vector<vector<int>>& result) {
+        for (int i=pos; i<candidates.size(); ++i) {
+            int cnd = candidates[i];
             if (target == cnd) {
                 options.push_back(cnd);
-                vector<int> found(options);
-                sort(found.begin(), found.end());
-                if (result.end() == find(result.begin(), result.end(), found)) {
-                    result.push_back(found);
-                }
+                //vector<int> found(options);
+                //sort(found.begin(), found.end());
+                //if (result.end() == find(result.begin(), result.end(), found)) {
+                result.push_back(options);
+                //}
                 options.pop_back();
             } else if (target > cnd) {
                 options.push_back(cnd);
-                checkResult(candidates, target-cnd, options, result);
+                checkResult(candidates, target-cnd, i, options, result);
                 options.pop_back();
             }
         }
@@ -23,7 +24,7 @@ public:
         
         vector< vector<int>> result;
         vector<int> selected;
-        checkResult(candidates, target, selected, result);
+        checkResult(candidates, target, 0, selected, result);
         
         return result;
         
