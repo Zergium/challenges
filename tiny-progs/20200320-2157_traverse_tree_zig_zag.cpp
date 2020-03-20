96ae2c1c5102244f418d0fdbd4e5da4b826ee734
Zergium <grwlfo@gmail.com>
2020-03-20 22:10:47 +0100

    working zigzag

--- tiny-progs/20200320-2157_traverse_tree_zig_zag.cpp
+++ tiny-progs/20200320-2157_traverse_tree_zig_zag.cpp
@@ -10,12 +10,19 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if (nullptr == root) return {};
         vector<vector<int>> result;
         deque<TreeNode*> current_level;
         deque<TreeNode*> next_level;
         current_level.push_back(root);
         int level_no = 0;
         while (!current_level.empty()) {
+//             cout << "---" << endl;
+//             for (auto x: current_level) {
+//                 cout << x->val << "; ";
+//             }
+//             cout << endl;
+            
             vector<int> level_node_values;
             while (!current_level.empty()) {
                 TreeNode* node = current_level.front();
@@ -23,15 +30,14 @@ public:
                 level_node_values.push_back(node->val);
                 auto fn_add_node = [&](TreeNode* node) {
                     if (node != nullptr) {
-                        if (level_no % 2 == 1) {
-                            next_level.push_back(node);
-                        } else {
-                            next_level.push_front(node);
-                        }
+                        next_level.push_back(node);
                     }
                 };
                 fn_add_node(node->left);
                 fn_add_node(node->right);
+            }
+            if (level_no % 2 == 1) {
+                reverse(level_node_values.begin(), level_node_values.end());
             }
             level_no++;
             result.push_back(level_node_values);
