7c5c65d965afb3b519dade963020f94958863de6
Zergium <grwlfo@gmail.com>
2020-03-20 22:02:57 +0100

    improved zigzag

--- tiny-progs/20200320-2157_traverse_tree_zig_zag.cpp
+++ tiny-progs/20200320-2157_traverse_tree_zig_zag.cpp
@@ -10,14 +10,17 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
         deque<TreeNode*> current_level;
         deque<TreeNode*> next_level;
         current_level.push_back(root);
         int level_no = 0;
         while (!current_level.empty()) {
+            vector<int> level_node_values;
             while (!current_level.empty()) {
                 TreeNode* node = current_level.front();
                 current_level.pop_front();
+                level_node_values.push_back(node->val);
                 auto fn_add_node = [&](TreeNode* node) {
                     if (node != nullptr) {
                         if (level_no % 2 == 1) {
@@ -26,11 +29,12 @@ public:
                             next_level.push_front(node);
                         }
                     }
-                }
+                };
                 fn_add_node(node->left);
                 fn_add_node(node->right);
             }
             level_no++;
+            result.push_back(level_node_values);
             current_level = next_level;
             next_level.clear();
         }
