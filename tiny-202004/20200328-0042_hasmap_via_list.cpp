aebf922a2f43a985564482d92668fff1aed29b5a
Zergium <grwlfo@gmail.com>
2020-03-28 01:10:38 +0100

    updated hashmap

--- tiny-202004/20200328-0042_hasmap_via_list.cpp
+++ tiny-202004/20200328-0042_hasmap_via_list.cpp
@@ -1,3 +1,14 @@
+class DB {
+public:    
+    string name;
+    DB(const string& na) : name(na) {
+        cout << " >> " << name << endl;
+    }
+    ~DB() {
+        cout << "   " << name  << " <<" << endl;
+    }
+};
+
 class MyHashMap {
 public:
 
@@ -9,7 +20,7 @@ public:
         int key;
         int val;
         node_t* next;
-    }
+    };
     
     vector<node_t*> m_backets;
     int m_num_backets;
@@ -18,19 +29,22 @@ public:
         return ((key << 5) + key + 0x1a2b3c4d);
     }
     
-    static int idx_of_key(int key) {
+    int idx_of_key(int key) {
         return calc_hash(key) % m_num_backets;
     }
     
     /** Initialize your data structure here. */
     MyHashMap() 
         : m_num_backets(100000)
-        , m_backets(m_num_backets, nullptr)
+        , m_backets(100000, nullptr)
         {
+        //DB("constructor");
     }
     
+       
     /** value will always be non-negative. */
     void put(int key, int value) {
+        //DB("put");
         int idx = idx_of_key(key);
         if (m_backets[idx] == nullptr) {
             m_backets[idx] = new node_t(key, value);
@@ -49,6 +63,7 @@ public:
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
+        //DB("get");
         int idx = idx_of_key(key);
         node_t* node = m_backets[idx];
         while (node != nullptr && node->key != key) {
@@ -63,11 +78,12 @@ public:
 
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
+        //DB("remove");
         int idx = idx_of_key(key);
         node_t* node = m_backets[idx];
         if (node != nullptr) {
             if (node->key == key) {
-                m_backets[idx] == node->next;
+                m_backets[idx] = node->next;
                 delete node;
             } else {
                 node_t* prev;
@@ -75,7 +91,7 @@ public:
                     prev = node;
                     node = node->next;
                 }
-                if (key == node->key) {
+                if (nullptr != node && key == node->key) {
                     prev->next = node->next;
                     delete node;
                 }
@@ -83,6 +99,19 @@ public:
         }
     }
 };
+
+
+auto speedup=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+/*
+["MyHashMap","put","put","get","get","put","get", "remove", "get"]
+[[],[1,1],[2,2],[1],[3],[2,1],[2],[2],[2]]
+*/
 
 /**
  * Your MyHashMap object will be instantiated and called as such:
