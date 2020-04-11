f8b3d2ac28922e39fea4a4b8969a0f66a9123dfc
Zergium <grwlfo@gmail.com>
2020-04-11 22:47:02 +0200

    fixed generate parenthesis

--- tiny-202004/20200411-2241_generateParenhesis.cpp
+++ tiny-202004/20200411-2241_generateParenhesis.cpp
@@ -25,26 +25,22 @@ public:
         // ((()))
         // 
 
-        if (n == 0) { return {}; }
-        unordered_set<string> variants;
-        variants.insert("()");
-        int i = 1;
-        while (i<n) {
-            unordered_set<string> next;
-            for (const auto& v: variants) {
-                next.insert(v + "()");
-                next.insert("()" + v);
-                next.insert("(" + v + ")");
-            }   
-            i++;
-            variants = move(next);
-        }
-        
         vector<string> result;
-        for (auto& v: variants) {
-            result.push_back(v);
-        }
+        addParenhesis(result, "", n, 0);
         return result;
         
+    }
+    
+    void addParenhesis(vector<string>& result, const string& curr, int left, int right) {
+        if (left == 0 && right == 0) {
+            result.push_back(curr);
+        } else {
+            if (left > 0) {
+                addParenhesis(result, curr + "(", left-1, right+1);
+            }
+            if (right > 0) {
+                addParenhesis(result, curr + ")", left, right-1);
+            }
+        }
     }
 };
