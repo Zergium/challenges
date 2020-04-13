4e54092b44b8021132eb63702f945f966ba1e8cf
Zergium <grwlfo@gmail.com>
2020-04-14 01:33:30 +0200

    updated find longest 0 1 sequence

--- april-challenge/20200414-0059_findMaxLength.cpp
+++ april-challenge/20200414-0059_findMaxLength.cpp
@@ -4,24 +4,32 @@ public:
         if (nums.size() == 0) {
             return 0;
         }
+       
+        // 0 1   = 2
+        // 0 0 0 1 = 2
+
+        // 1111111110101011111111111111110101010101001010010101001
+        // 12345678989898
+        // 00000000000000
+        // 00000000011223
+        
         int best = 0;
-        int prev_amount = 0;
-        int check_digit = nums[0];
-        int idx = 0;
-        while (idx < nums.size()) {
-            int amount = 0;
-            while (idx < nums.size() && check_digit == nums[idx]) {
-                idx++;
-                amount++;
+        
+        unordered_map<int, int> map_sums_to_idx;
+        int sum = 0;
+        map_sums_to_idx[sum] = -1;
+        for(int idx = 0; idx < nums.size(); idx++) {
+            sum += (nums[idx] == 0 ? -1 : 1);
+            auto it_sum = map_sums_to_idx.find(sum);
+            if (it_sum == map_sums_to_idx.end()) {
+                map_sums_to_idx[sum] = idx;    
+            } else {
+                int prev_idx = it_sum->second;
+                int len = idx - prev_idx;
+                if (best < len) best = len;
             }
-            int current = prev_amount + amount;
-            if (current > best) {
-                best = current;
-            }
-            
-            prev_amount = amount;
-            check_digit = (check_digit + 1) % 2;
         }
+        
         return best;
     }
 };
