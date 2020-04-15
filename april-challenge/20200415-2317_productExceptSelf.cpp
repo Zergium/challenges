b05cc3293359e7419e5a1a058023cfc3cad36b17
Zergium <grwlfo@gmail.com>
2020-04-15 23:22:53 +0200

    updated productExceptSelf

--- april-challenge/20200415-2317_productExceptSelf.cpp
+++ april-challenge/20200415-2317_productExceptSelf.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> onleft(n);
-        vector<int> onright(n);
-        onleft[0] = nums[0];
-        onright[n-1] = nums[n-1];
-        for (int i=1; i<n; i++) {
-            onleft[i] = onleft[i-1] * nums[i];
-            onright[n-1-i] = onright[n-1-i+1] * nums[n-1-i];
-        }
         vector<int> result(n);
-        result[0] = onright[1];
-        result[n-1] = onleft[n-1-1];
-        for (int i=1; i<n-1; i++) {
-            result[i] = onleft[i-1] * onright[i+1];
+        int acc = 1;
+        for (int i = 0; i<n; i++) {
+            result[i] = acc;
+            acc *= nums[i];
         }
-        return result;
+        acc = 1;
+        for (int i = n-1; i >= 0; i--) {
+            result[i] *= acc;
+            acc *= nums[i];
+        }
+       return result;
     }
 };
