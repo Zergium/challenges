90d264b46f4841dca2775449016a582496e429e0
Zergium <grwlfo@gmail.com>
2020-04-17 22:24:02 +0200

    memoization 50x boost for check string

--- april-challenge/20200417-2132_checkStringValidness.cpp
+++ april-challenge/20200417-2132_checkStringValidness.cpp
@@ -34,3 +34,54 @@ public:
 };
 
 
+=============== memoization
+
+// 12 ms
+
+class Solution {
+public:
+    //typedef pair<int, int> posi_t;
+    unordered_map<int, bool> m_prevRuns;
+    
+    bool isValidRest(const string& s, int idx, int numRight) {
+        
+        // memois
+        int key = idx*1000 + numRight;
+        auto it = m_prevRuns.find(key);
+        if (it != m_prevRuns.end()) {
+            return it->second;
+        }
+    
+        // result
+        bool result = false;
+        
+        if (idx == s.size()) {
+            result = (0 == numRight);
+        } else  if (s[idx] == '(') {
+            result = isValidRest(s, idx+1, numRight+1);
+        } else if (s[idx] == ')') {
+            if (numRight == 0) {
+                result = false;
+            } else {
+                result = isValidRest(s, idx+1, numRight-1);
+            }
+        } else {
+            // ch = '*'
+            result = isValidRest(s, idx+1, numRight+1);
+            if (!result && numRight > 0) {
+                result = isValidRest(s, idx+1, numRight-1);
+            }
+            if (!result) {
+                result = isValidRest(s, idx+1, numRight);
+            }
+        }
+        m_prevRuns[key] = result;
+        return result;
+    }
+    
+    bool checkValidString(string s) {
+        return isValidRest(s, 0, 0);
+    }
+};
+
+
