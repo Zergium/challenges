dde9b84b8d6abf9c7488593317d92578238dac8e
Zergium <grwlfo@gmail.com>
2020-04-21 23:50:27 +0200

    simplified is palindrome

--- tiny-202004/20200421-2344_isPalindrome.cpp
+++ tiny-202004/20200421-2344_isPalindrome.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        stringstream ss;
-        for (char ch: s) {
-            ch = tolower(ch);
-            if ('a' <= ch && ch <= 'z' || '0' <= ch && ch <= '9') {
-                ss << ch;
-            }
-        }
-        string filtered = ss.str();
-        //cout << "filtered: " << filtered << endl;
-        int half_size = filtered.size()/2;
-        for (int i=0; i<half_size; i++) {
-            //cout << filtered[i] << " vs " << filtered[s.size()-1-i] << endl;
-            if (filtered[i] != filtered[filtered.size()-1-i]) {
-                return false; 
-            }
+        int lo = 0;
+        int hi = s.size()-1;
+        while (lo < hi) {
+            while (lo < hi && !isalnum(s[lo])) lo++;
+            while (lo < hi && !isalnum(s[hi])) hi--;
+            if (tolower(s[lo]) != tolower(s[hi])) return false;
+            lo++;
+            hi--;
         }
         return true;
     }
