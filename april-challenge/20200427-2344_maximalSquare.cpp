a1af540d3df354d1adef2b4eecdb9900c0fdce87
Zergium <grwlfo@gmail.com>
2020-04-28 00:20:56 +0200

    updated max square

--- april-challenge/20200427-2344_maximalSquare.cpp
+++ april-challenge/20200427-2344_maximalSquare.cpp
@@ -38,3 +38,45 @@ public:
     }
 };
 
+// optimum
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int h = matrix.size();
+        if (h<1) return 0;
+        int w = matrix[0].size();
+        if (w<1) return 0;
+        vector< vector<int> > dat(h, vector<int>(w));
+        
+        for (int x=0; x<w; x++) {
+            dat[0][x] = matrix[0][x] - '0';
+        }
+        for (int y=1; y<h; y++) {
+            dat[y][0] = matrix[y][0] - '0';
+        }
+        
+        for (int y=1; y<h; y++) {
+            for (int x=1; x<w; x++) {
+                if (matrix[y][x] == '1') {
+                    dat[y][x] = 1 + min(min(dat[y-1][x],dat[y-1][x-1]), dat[y][x-1]);
+                }
+            }
+        }
+        
+        // for (auto& ar: dat) {
+        //     for (auto v: ar) {
+        //         cout << v << " ";
+        //     }
+        //     cout << endl;
+        // }
+        
+        int largest_square(0);
+        for (int y=0; y<h; y++) {
+            for (int x=0; x<w; x++) {
+                largest_square = max(largest_square, dat[y][x]*dat[y][x]);
+            }
+        }
+        
+        return largest_square;
+    }
+};
