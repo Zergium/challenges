d9353916f9ae6f21df5fc699662d6c3b16a00596
Zergium <grwlfo@gmail.com>
2020-04-28 23:55:08 +0200

    first unique

---
+++ april-challenge/20200428-2341_firstUnique.cpp
@@ -0,0 +1,42 @@
+class FirstUnique {
+public:
+    list<int> unique;
+    unordered_map<int, list<int>::iterator> unique_ptr;
+    
+    unordered_set<int> multiple;
+    FirstUnique(vector<int>& nums) {
+        for (int num: nums) {
+            add(num);
+        }
+    }
+    
+    int showFirstUnique() {
+        if (unique.empty()) return -1;
+        return unique.front();
+    }
+    
+    void add(int value) {
+        
+        if (multiple.count(value) > 0) {
+            return;
+        }
+        
+        if (unique_ptr.count(value) > 0) {
+            multiple.insert(value);
+            unique.erase(unique_ptr[value]);
+            unique_ptr.erase(value);
+        } else {
+            unique.push_back(value);
+            unique_ptr[value] = prev(unique.end());
+        }
+    }
+};
+
+/**
+ * Your FirstUnique object will be instantiated and called as such:
+ * FirstUnique* obj = new FirstUnique(nums);
+ * int param_1 = obj->showFirstUnique();
+ * obj->add(value);
+ */
+
+
